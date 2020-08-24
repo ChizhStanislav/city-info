2732c782b3e8cdfd37e8af3750ccf18eb400b46a
ChizhStanislav <stas23041991@gmail.com>
2020-08-24 21:51:18 +0300

    Edit ControllerExceptionHandler and City

--- src/main/java/by/chyzh/cityinfo/controller/ControllerExceptionHandler.java
+++ src/main/java/by/chyzh/cityinfo/controller/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package by.chyzh.cityinfo.controller;
 
 import by.chyzh.cityinfo.exception.NotFoundException;
+import org.hibernate.PropertyValueException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +14,16 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> notFoundExceptionHandler(NotFoundException notFoundException) {
         return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PropertyValueException.class)
+    public ResponseEntity<String> propertyValueExceptionHandler(PropertyValueException propertyValueException) {
+        return new ResponseEntity<>("not-null", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> dataIntegrityViolationExceptionHandler(DataIntegrityViolationException dataIntegrityViolationException) {
+        return new ResponseEntity<>("not-unique", HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/by/chyzh/cityinfo/entity/City.java
+++ src/main/java/by/chyzh/cityinfo/entity/City.java
@@ -10,7 +10,6 @@ import javax.persistence.Table;
 @Builder
 @Setter
 @Getter
-@ToString(exclude = "")
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
